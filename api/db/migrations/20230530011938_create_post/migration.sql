179b2234736f18cc4f356e8d8d0de35844a47d51
POWERUSER <tyresehutcheson@gmail.com>
2023-05-29 21:23:18 -0400

    Fixed issue with creating posts

---
+++ api/db/migrations/20230530011938_create_post/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - Added the required column `body` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `title` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_Post" ("id") SELECT "id" FROM "Post";
+DROP TABLE "Post";
+ALTER TABLE "new_Post" RENAME TO "Post";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
